c60e60d57405e1de7158ca4952bb23b294ed6afb
nicolo fossati <nicofossa01@gmail.com>
2023-03-14 14:12:50 +0100

    BONUS

--- BONUS/js/main.js
+++ BONUS/js/main.js
@@ -8,7 +8,8 @@ Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 createApp({
     data() {
         return {
-            messaggio: 'Questo è il messaggio con VUE JS'
+            messaggio: 'Questo è il messaggio con VUE JS',
+            img_url: 'https://cdn.pixabay.com/photo/2016/06/15/15/02/info-1459077_960_720.png'
         }
     }
 }).mount('#app')

809b4f9cac86a0cd7b503e2560971440afbacb73
nicolo fossati <nicofossa01@gmail.com>
2023-03-14 14:01:59 +0100

    esercizio

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 const { createApp } = Vue
-
+/*
+Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+*/
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            messaggio: 'Questo è il messaggio'
         }
     }
 }).mount('#app')
